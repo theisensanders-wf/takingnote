43d11ef8c3a08452866b48ec581c16bb31d3efb0
Theisen Sanders <theisen.sanders@gmail.com>
2013-08-30 11:24:40 -0500

    Added switch directive

--- static/js/directives.js
+++ static/js/directives.js
@@ -3,11 +3,40 @@ angular.module('directives', [])
         return {
             restrict: 'E', // Attribute
             template: '<input type="file">',
-            link: function ($scope, $element) {
+            link: function ($scope, $element, $attrs) {
                 var fileInput = $element.find('input[type="file"]');
                 fileInput.bind('change', function (e) {
                     $fileUpload.upload(e.target.files[0]);
                 });
             }
         }
-    });
+    })
+    .directive('switch', function () {
+        return {
+            template: '<div class="make-switch" ng-class="size"><input ng-model="ngModel" type="checkbox"></div>',
+            replace: true,
+            require: 'ngModel',
+            scope: {
+                ngModel: '=',
+                size: '@'
+            },
+            link: function (scope, element, attrs) {
+                scope.$watch('ngModel', function(newValue) {
+                    element.bootstrapSwitch('setState', newValue);
+                });
+
+                scope.$watch('size', function(newValue) {
+                    element.bootstrapSwitch('setSizeClass', newValue);
+                });
+
+                element.on('switch-change', function(event, data) {
+                    var value = data.value;
+                    scope.ngModel = value;
+                    scope.$apply();
+                });
+
+            }
+        }
+    }
+
+);
